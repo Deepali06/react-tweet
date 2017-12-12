fb7f9de8007cb8f2de29c502a33c5c6135515f46
AKHILESHWAR RACHAKONDA <akhileshwar.rachakonda1@wipro.com>
2017-12-12 21:02:29 +0530

    sample test

---
+++ test/testspec.js
@@ -0,0 +1,10 @@
+const should = require("chai").should(),
+assert = require ("chai").assert,
+
+describe('sample', function() {
+  describe('sample desc', function() {
+    it('passed test', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
